9d2a5698e74c60b32130124577344d9ad32419da
mohammadHidayat <mohammadhidayat8@gmail.com>
2023-01-23 18:44:14 +0600

    navbar and hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./Page/Homepage/HeroSection/HeroSection";
 import NavbarSection from "./Page/Homepage/NavbarSection/NavbarSection";
 
 function App() {
   return (
     <div>
       <NavbarSection></NavbarSection>
+      <HeroSection></HeroSection>
     </div>
   );
 }
---
+++ src/Page/Homepage/HeroSection/HeroSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
+      >
+        <div className="hero-overlay bg-opacity-60 bg-green-400"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-2xl">
+            <h1 className="mb-5 text-5xl font-bold uppercase text-black">
+              We Help Your Business
+            </h1>
+            <p className="mb-5 text-black">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout.
+            </p>
+            <button className="btn bg-white text-black uppercase">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
