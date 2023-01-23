6344adbbb468423236175a16c191855933cb6f95
mohammadHidayat <mohammadhidayat8@gmail.com>
2023-01-23 18:23:00 +0600

    initial files ready

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavbarSection from "./Page/Homepage/NavbarSection/NavbarSection";
+
+function App() {
+  return (
+    <div>
+      <NavbarSection></NavbarSection>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Homepage/NavbarSection/NavbarSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarSection = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-6xl">Oiii</h1>
+    </div>
+  );
+};
+
+export default NavbarSection;
